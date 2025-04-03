4999ebbaa853edb1fdd7ff9ab5d731dbbfda79f3
Tobias Ahlmark <din@adress.comOvrarphaga@gmail.com>
2025-04-03 18:14:59 +0200

    Lagt till ett första enhetstest

--- WebApiAdmin/obj/Debug/net9.0/webapiadmin.AssemblyInfo.cs
+++ WebApiAdmin/obj/Debug/net9.0/webapiadmin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapiadmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c77d32f0b2b4fc70286b79734e10d4a777747947")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf618d52af00a8f1b1c55ed308e575e350652cf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapiadmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapiadmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
